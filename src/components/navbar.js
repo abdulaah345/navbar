bcd52ba9f1c9dc626f36cc01e9655084c5dd6a1d
Your Name <you@example.com>
2024-04-07 01:36:28 +0200

    create navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <img src={'https://logowik.com/content/uploads/images/257_al_ahly_club.jpg'} style={{width:200 }} alt='Logo'/>
+  )
+}
---
+++ src/content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Content = () => {
+  return (
+    <div>
+        <p>some info about react:</p>
+        <ul>
+            <li>React fact1</li>
+            <li>React fact2</li>
+            <li>React fact3</li>
+            <li>React fact4</li>
+        </ul>
+        
+        </div>
+  )
+}
